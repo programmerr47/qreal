558211dfbfe5c4b12b93314f18910ab8d391c6d0
programmerr47 <spitsinmichael@gmail.com>
2012-05-10 20:47:32 +0400

    Implemented mainWindowControllerApi

--- qrgui/mainwindow/mainWindowControllerApi.h
+++ qrgui/mainwindow/mainWindowControllerApi.h
@@ -1,10 +1,22 @@
 #ifndef MAINWINDOWCONTROLLERAPI_H
 #define MAINWINDOWCONTROLLERAPI_H
 
-class mainWindowControllerApi
+#include <QtCore/QString>
+
+#include "../pluginManager/editorManager.h"
+
+class MainWindowControllerApi
 {
 public:
-    mainWindowControllerApi();
+    MainWindowControllerApi(qReal::EditorManager &editorManager);
+    QString propertyDescription(const qReal::Id &id, const QString &propertyName) const;
+    QString propertyDisplayedName(const qReal::Id &id, const QString &propertyName) const;
+    QStringList getEnumValues(const qReal::Id &id, const QString &propertyName) const;
+    QStringList getPropertyNames(const qReal::Id &id) const;
+    QString getTypeName(const qReal::Id &id, const QString &propertyName) const;
+
+private:
+    qReal::EditorManager &mEditorManager;
 };
 
 #endif // MAINWINDOWCONTROLLERAPI_H
